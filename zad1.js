1186a8f435c73e733b22727aaa9ce48ac3e57cc5
Ganer03 <112845740+Ganer03@users.noreply.github.com>
2022-11-19 01:08:32 +0300

    Update zad1.js

--- zad1.js
+++ zad1.js
@@ -1,1 +1,24 @@
+function followLink(event, link)
+{
+	var nameLink = link.innerHTML;
+	uploadContent(link.href);
+	window.history.pushState({title:nameLink}, null, "form");
+	updateTitle(nameLink);
+	event.preventDefault();
+}
 
+function updateTitle(title)
+{
+	var elm = document.getElementsByTagName('title')[0];
+	elm.innerHTML = title;
+}
+
+function uploadContent(link)
+{
+	//тут реализуем загрузку части страницы с помощью AJAX
+}
+
+window.addEventListener("popstate", function(e) {
+	uploadContent(e.state.href);									 
+	updateTitle(e.state.title);
+}, false );
